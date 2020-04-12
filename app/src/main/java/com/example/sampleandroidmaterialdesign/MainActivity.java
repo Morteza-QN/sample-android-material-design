d0e965854761679f01c03f39ff00f096699635d6
morteza <mortezaghorbani3@gmail.com>
2020-04-12 15:23:44 +0430

    show toolbar

--- app/src/main/java/com/example/sampleandroidmaterialdesign/MainActivity.java
+++ app/src/main/java/com/example/sampleandroidmaterialdesign/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sampleandroidmaterialdesign;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar_main_activity));
     }
 }
