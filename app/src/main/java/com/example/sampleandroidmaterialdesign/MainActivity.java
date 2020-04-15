fe10e6fe0d82c33b1c550845b3e1781754af714c
morteza <mortezaghorbani3@gmail.com>
2020-04-15 21:13:30 +0430

    floatingActionButton added

--- app/src/main/java/com/example/sampleandroidmaterialdesign/MainActivity.java
+++ app/src/main/java/com/example/sampleandroidmaterialdesign/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sampleandroidmaterialdesign;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_main_fragmentContainer, new MainFragment());
+        fragmentTransaction.commit();
+        //first fragment don't add backStack
 
         //setSupportActionBar((Toolbar) findViewById(R.id.tb_main));
         //set toolbar to activity
--- app/src/main/java/com/example/sampleandroidmaterialdesign/MainFragment.java
+++ app/src/main/java/com/example/sampleandroidmaterialdesign/MainFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainFragment extends Fragment {
 
@@ -37,10 +39,18 @@ public class MainFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.add(R.id.frame_main_fragmentContainer, new DetailFragment());
+                fragmentTransaction.replace(R.id.frame_main_fragmentContainer, new DetailFragment());
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
                 //ocClickListener for cardView
+            }
+        });
+
+        FloatingActionButton fab = view.findViewById(R.id.fab_main_fragment);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "fab clocked", Toast.LENGTH_SHORT).show();
             }
         });
     }
